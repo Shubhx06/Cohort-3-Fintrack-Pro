69b1a2d61ded56468b9b8decc6ea4b9f1e566387
Shubham Mahato <112699758+Shubhx06@users.noreply.github.com>
2026-07-01 02:13:25 +0530

    Refactor login.js to add registration and login functionality

--- login.js
+++ login.js
@@ -2,8 +2,138 @@ const registerLinkBtn = document.querySelector(".registerLink");
 const loginPage = document.querySelector("#loginPage");
 const registerPage = document.querySelector("#registerPage");
 const loginLinkBtn = document.querySelector(".loginLink");
-const loginBtn = document.querySelector(".loginBtn");
 
+
+
+
+
+
+
+
+const registerBtn = document.querySelector(".registerBtn")
+const registerInputsDiv = document.querySelector("#registerInputsDiv")
+const registerUsername = document.querySelector("#registerUsername")
+const registerPassword = document.querySelector("#registerPassword")
+
+
+
+const inputsDiv = document.querySelector(".inputsDiv")
+const loginUsername = document.querySelector("#loginUsername")
+const loginPassword = document.querySelector("#loginPassword")
+const loginBtn = document.querySelector(".loginBtn")
+
+let users = JSON.parse(localStorage.getItem("idPass")) || [];
+
+let loggedInUser  = localStorage.getItem("currentUser");
+
+if(loggedInUser ){
+    
+    loginPage.classList.add("hidden");
+    registerPage.classList.add("hidden");
+}
+
+
+registerInputsDiv.addEventListener("submit",(e)=>{
+
+ e.preventDefault();
+
+
+ let username = registerUsername.value.trim();
+ let password =registerPassword.value.trim();
+
+if (registerUsername.value === "" || registerPassword.value === "") {
+    return;
+}
+
+let obj1 = {
+password,
+username
+}
+
+let exists = users.find(user => user.username === registerUsername.value);
+
+if (exists) {
+    alert("Username already exists");
+    return;
+}
+
+
+
+ Swal.mixin({
+  toast: true,
+  position: "top-end",
+  showConfirmButton: false,
+  timer: 3000,
+  timerProgressBar: true,
+  didOpen: (toast) => {
+    toast.onmouseenter = Swal.stopTimer;
+    toast.onmouseleave = Swal.resumeTimer;
+  }
+}).fire({
+  icon: "success",
+  title: "Register is successfully"
+});
+
+
+
+users.push(obj1)
+
+localStorage.setItem("idPass",JSON.stringify(users))
+ registerPage.classList.add("hidden");
+ loginPage.classList.remove("hidden");
+     
+
+})
+
+
+
+
+
+
+
+inputsDiv.addEventListener("submit",(e)=>{
+ e.preventDefault();
+  
+
+ let foundUser = users.find(user => {
+    return (
+        user.username === loginUsername.value &&
+        user.password === loginPassword.value
+    );
+ });
+
+if (foundUser) {
+    
+
+ Swal.mixin({
+  toast: true,
+  position: "top-end",
+  showConfirmButton: false,
+  timer: 3000,
+  timerProgressBar: true,
+  didOpen: (toast) => {
+    toast.onmouseenter = Swal.stopTimer;
+    toast.onmouseleave = Swal.resumeTimer;
+  }
+}).fire({
+  icon: "success",
+  title: "Signed in successfully"
+});
+
+localStorage.setItem("currentUser", foundUser.username);
+location.reload()
+console.log("Login Successful");
+
+loginPage.classList.add("hidden");
+    registerPage.classList.add("hidden");
+
+}else {
+    alert("Wrong username or password");
+}
+
+
+
+})
 
 registerLinkBtn.addEventListener("click", () => {
     loginPage.classList.add("hidden");
@@ -16,9 +146,19 @@ loginLinkBtn.addEventListener("click", () => {
 });
 
 
-loginBtn.addEventListener("click", (e) => {
-    e.preventDefault();
+const logoutBtn = document.querySelector(".logoutBtn");
 
-    loginPage.classList.add("hidden");
-    registerPage.classList.add("hidden");
+logoutBtn.addEventListener("click",()=>{
+
+    localStorage.removeItem("currentUser");
+
+    location.reload();
+
 });
+
+// loginBtn.addEventListener("click", (e) => {
+//     e.preventDefault();
+
+//     loginPage.classList.add("hidden");
+//     registerPage.classList.add("hidden");
+// });
